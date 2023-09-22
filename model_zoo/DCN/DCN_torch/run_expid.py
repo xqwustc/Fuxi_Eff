d53a3c960d7b2f67c67c999f45f151c68067ff19
Wang Xianquan <wxq2001@gmail.com>
2023-09-22 17:35:27 +0800

    <feat> append sys

--- model_zoo/DCN/DCN_torch/run_expid.py
+++ model_zoo/DCN/DCN_torch/run_expid.py
@@ -16,11 +16,13 @@
 
 
 import os
+import sys
+sys.path.append(".../fuxictr")
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 import sys
 import logging
-import fuxictr_version
-from fuxictr import datasets
+# from fuxictr import datasets
 from datetime import datetime
 from fuxictr.utils import load_config, set_logger, print_to_json, print_to_list
 from fuxictr.features import FeatureMap
