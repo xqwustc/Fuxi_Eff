5303700a03945d38ee4daf6422c10d1be78f4047
Wang Xianquan <wxq2001@gmail.com>
2023-09-23 21:54:44 +0800

    <feat> add selector arch

---
+++ feat_select/BaseSelector.py
@@ -0,0 +1,11 @@
+class BaseSelector:
+    # This module will be wrapped around the outer layer of the CTR training model for feature selection, ultimately
+    # returning the importance (in descending order) and feature IDs of each feature.
+    def __init__(self,model,inputs,feature_map):
+        # Initialize the selector
+        self.model = model  # model for training
+        self.inputs = inputs
+        self.feature_map = feature_map
+
+    def select(self):
+        pass
