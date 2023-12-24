5d1b7d2e4ebdebbc2515996671d1a0e22bc2b2fc
Wang Xianquan <wxq2001@gmail.com>
2023-12-24 16:42:11 +0800

    <feat> update DIN and add droprank

--- model_zoo/DIN/src/DIN.py
+++ model_zoo/DIN/src/DIN.py
@@ -20,6 +20,11 @@ import numpy as np
 from pandas.core.common import flatten
 from fuxictr.pytorch.models import BaseModel
 from fuxictr.pytorch.layers import FeatureEmbeddingDict, MLP_Block, DIN_Attention, Dice
+import logging
+import sys
+import pandas as pd
+from tqdm import tqdm
+from model_zoo.utils import get_gates_prob_my
 
 
 class DIN(BaseModel):
@@ -49,6 +54,7 @@ class DIN(BaseModel):
                                   embedding_regularizer=embedding_regularizer, 
                                   net_regularizer=net_regularizer,
                                   **kwargs)
+        self.learning_rate = learning_rate
         if not isinstance(din_target_field, list):
             din_target_field = [din_target_field]
         self.din_target_field = din_target_field
@@ -100,6 +106,31 @@ class DIN(BaseModel):
         return_dict = {"y_pred": y_pred}
         return return_dict
 
+    def forward_with_dr(self,inputs,gates_prob):
+        X = self.get_inputs(inputs)
+        feature_emb_dict = self.embedding_layer(X)
+        assert self.feature_map.features.keys() == feature_emb_dict.keys(), \
+            "feature_map.features.keys() != feature_emb_dict.keys(), cannot use gates directly"
+
+        # gates * each emb
+        for idx,(field_name,emb) in enumerate(feature_emb_dict.items()):
+            feature_emb_dict[field_name] = gates_prob[idx] * emb
+
+        for idx, (target_field, sequence_field) in enumerate(zip(self.feature_map.features.keys(),
+                                                                 feature_emb_dict.keys())):
+            target_emb = self.get_embedding(target_field, feature_emb_dict)
+            sequence_emb = self.get_embedding(sequence_field, feature_emb_dict)
+            seq_field = list(flatten([sequence_field]))[0] # flatten nested list to pick the first sequence field
+            mask = X[seq_field].long() != 0 # padding_idx = 0 required
+            pooling_emb = self.attention_layers[idx](target_emb, sequence_emb, mask)
+            for field, field_emb in zip(list(flatten([sequence_field])),
+                                        pooling_emb.split(self.embedding_dim, dim=-1)):
+                feature_emb_dict[field] = field_emb
+        feature_emb = self.embedding_layer.dict2tensor(feature_emb_dict, flatten_emb=True)
+        y_pred = self.dnn(feature_emb)
+        return_dict = {"y_pred": y_pred}
+        return return_dict
+
     def get_embedding(self, field, feature_emb_dict):
         if type(field) == tuple:
             emb_list = [feature_emb_dict[f] for f in field]
@@ -107,3 +138,98 @@ class DIN(BaseModel):
         else:
             return feature_emb_dict[field]
 
+    def fit_for_dr(self, data_generator, epochs=1, validation_data=None,
+            max_gradient_norm=10., **kwargs):
+        self.valid_gen = validation_data
+        self._max_gradient_norm = max_gradient_norm
+        self._best_metric = np.Inf if self._monitor_mode == "min" else -np.Inf
+        self._stopping_steps = 0
+        self._steps_per_epoch = len(data_generator)
+        self._stop_training = False
+        self._total_steps = 0
+        self._batch_index = 0
+        self._epoch_index = 0
+        if self._eval_steps is None:
+            self._eval_steps = self._steps_per_epoch
+
+        # Make a list of theta for each feature
+        gates_theta = torch.ones(len(self.feature_map.features)) * 0.5
+        gates_theta.requires_grad_(requires_grad=True)
+
+        gates_sigma = torch.ones(len(self.feature_map.features)) * 0.5
+        gates_sigma.requires_grad_(requires_grad=True)
+
+        # --- update for droprank start---
+        self.optimizer.add_param_group({'params': gates_theta, 'lr': self.learning_rate*0.1})
+        self.optimizer.add_param_group({'params': gates_sigma, 'lr': self.learning_rate*0.1})
+        # --- update for droprank end---
+
+        logging.info("Start training: {} batches/epoch".format(self._steps_per_epoch))
+        logging.info("************ Epoch=1 start ************")
+        for epoch in range(epochs):
+            self._epoch_index = epoch
+            self._batch_index = 0
+            train_loss = 0
+            self.train()
+            if self._verbose == 0:
+                batch_iterator = data_generator
+            else:
+                batch_iterator = tqdm(data_generator, disable=False, file=sys.stdout)
+            for batch_index, batch_data in enumerate(batch_iterator):
+                self._batch_index = batch_index
+                self._total_steps += 1
+
+                # --- update for droprank start---
+                gates_prob = get_gates_prob_my(gates_theta, gates_sigma)
+                return_dict = self.forward_with_dr(batch_data, gates_prob)
+                # --- update for droprank end---
+
+                self.optimizer.zero_grad()
+
+                y_true = self.get_labels(batch_data)
+                loss = self.compute_loss(return_dict, y_true)
+
+                # --- update for droprank start---
+                loss += (torch.sum(gates_prob)) * 1e-5
+
+                ####
+                # dot = make_dot(loss, params=dict(self.named_parameters()))
+                # dot.view()
+                ####
+
+                # --- update for droprank end---
+
+                loss.backward()
+
+                # # --- update for droprank start---
+                # print("\n",gates_theta.grad,"\n")
+                # # --- update for droprank end---
+
+                nn.utils.clip_grad_norm_(self.parameters(), self._max_gradient_norm)
+                self.optimizer.step()
+
+                train_loss += loss.item()
+                if self._total_steps % self._eval_steps == 0:
+                    logging.info("Train loss: {:.6f}".format(train_loss / self._eval_steps))
+                    train_loss = 0
+                    # eval the model
+                    self.eval_step()
+                if self._stop_training:
+                    break
+
+                # --- update for droprank start---
+            logging.info("\nMy Gates Theta: {}".format(gates_theta))
+            # --- update for droprank end---
+
+            if self._stop_training:
+                break
+            else:
+                logging.info("************ Epoch={} end ************".format(self._epoch_index + 1))
+        logging.info("Training finished.")
+        logging.info("Load best model: {}".format(self.checkpoint))
+        self.load_weights(self.checkpoint)
+        print(gates_theta)
+        feature_importance_result = pd.DataFrame({'feature_name': list(self.feature_map.features.keys()),
+                                                  'feature_weight': gates_theta.tolist(),
+                                                  'feature_sigma': gates_sigma.tolist()})
+        feature_importance_result.to_csv('feature_importance_result.csv', index=False)
