18874c1af4af8144e1c45bad33eb0c5eddb8aeb1
Wang Xianquan <wxq2001@gmail.com>
2023-12-25 15:50:41 +0800

    <feat> add mvfs for DNN

---
+++ model_zoo/DNN/DNN_torch/run_expid_mvfs_incre.py
@@ -0,0 +1,130 @@
+# =========================================================================
+# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
+
+
+import os
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+import sys
+
+sys.path.append('../../../fuxictr')
+sys.path.append('../../../')
+# print(sys.path)
+
+import logging
+import pickle
+import fuxictr
+import fuxictr_version
+from fuxictr import datasets
+from datetime import datetime
+from fuxictr.utils import load_config, set_logger, print_to_json, print_to_list
+from fuxictr.features import FeatureMap
+from fuxictr.pytorch.torch_utils import seed_everything
+from fuxictr.pytorch.dataloaders import H5DataLoader
+from fuxictr.preprocess import build_dataset
+import src as model_zoo
+import gc
+import argparse
+import os
+from pathlib import Path
+import importlib
+import torch
+import numpy as np
+import logging
+from infomer import email
+import pandas as pd
+
+if __name__ == '__main__':
+    ''' Usage: python run_expid.py --config {config_dir} --expid {experiment_id} --gpu {gpu_device_id}
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='./config/', help='The config directory.')
+    parser.add_argument('--expid', type=str, default='DeepFM_test', help='The experiment id to run.')
+    parser.add_argument('--gpu', type=int, default=-1, help='The gpu index, -1 for cpu')
+    parser.add_argument('--cp', type=str, help='checkpoint path')
+    args = vars(parser.parse_args())
+
+    experiment_id = args['expid']
+    params = load_config(args['config'], experiment_id)
+    params['gpu'] = args['gpu']
+    set_logger(params)
+    logging.info("Params: " + print_to_json(params))
+    # seed_everything(seed=params['seed'])
+
+    if params.get('spe_processor',None) != None:
+        module_name = f"fuxictr.datasets.{params['spe_processor']}"
+        fp_module = importlib.import_module(module_name)
+        assert hasattr(fp_module, 'FeatureProcessor')
+        FeatureProcessor = getattr(fp_module, 'FeatureProcessor')
+    else:
+        from fuxictr.preprocess import FeatureProcessor
+
+    data_dir = os.path.join(params['data_root'], params['dataset_id'])
+    feature_map_json = os.path.join(data_dir, "feature_map.json")
+    if params["data_format"] == "csv":
+        # Build feature_map and transform h5 data
+        feature_encoder = FeatureProcessor(**params)
+        params["train_data"], params["valid_data"], params["test_data"] = \
+            build_dataset(feature_encoder, **params)
+    feature_map = FeatureMap(params['dataset_id'], data_dir)
+    feature_map.load(feature_map_json, params)
+
+    select_nums = []
+    AUCs = []
+    logloss = []
+    for i in range(0, len(feature_map.features)):
+        model_class = getattr(model_zoo, params['model'])
+        print('params[model]', params['model'])
+        model = model_class(feature_map, select_num = i+1,**params)
+        select_nums.append(i+1)
+        model.count_parameters()  # print number of parameters used in model
+
+        print('Used Feature Number:', i + 1)
+        train_gen, valid_gen = H5DataLoader(feature_map, stage='train', **params).make_iterator()
+        # email.common_send('AdaFS_select.py - Build Data',"")
+        if args.get('cp',None) != None:
+            print('load model from checkpoint')
+            model.load_state_dict(torch.load(args['cp']))
+        else:
+            model.fit_for_mvfs(train_gen, validation_data=valid_gen, **params)
+
+        # email.common_send('AdaFS_{}_select.py - Training', "".format(args['expid']))
+
+        logging.info('****** Validation evaluation ******')
+        valid_result = model.evaluate(valid_gen)
+        del train_gen, valid_gen
+        gc.collect()
+
+        logging.info('****** MvFs Validation evaluation ******')
+        train_gen, valid_gen = H5DataLoader(feature_map, stage='train', **params).make_iterator()
+        model.evaluate(valid_gen)
+        del train_gen, valid_gen
+        gc.collect()
+
+        logging.info('******** Test evaluation ********')
+        test_gen = H5DataLoader(feature_map, stage='test', **params).make_iterator()
+        test_result = {}
+        if test_gen:
+            test_result = model.evaluate(test_gen)
+            AUCs.append(test_result['AUC'])
+            logloss.append(test_result['logloss'])
+        del test_gen
+
+    # Save the result
+    topk_ablation_df = pd.DataFrame({'feature_num': select_nums,
+                                     'topk_logloss': logloss,
+                                     'topk_auc': AUCs})
+    topk_ablation_df.to_csv('feature_ablation.csv')
--- model_zoo/DNN/DNN_torch/src/DNN.py
+++ model_zoo/DNN/DNN_torch/src/DNN.py
@@ -21,28 +21,32 @@ from fuxictr.pytorch.models import BaseModel
 from fuxictr.pytorch.layers import FeatureEmbedding, MLP_Block
 import numpy as np
 from itertools import cycle
-from model_zoo.utils import get_gates_prob_autofield,get_gates_prob_my
+from model_zoo.utils import get_gates_prob_autofield,get_gates_prob_my,get_sum_feature_dimisions
 from torch import optim
 import logging
 import sys
 import pandas as pd
 from tqdm import tqdm
+from itertools import cycle
+import torch.optim as optim
+import feat_select.MvFS_module as Mv
 EPS = 1e-6
 
 
 
 class DNN(BaseModel):
-    def __init__(self, 
-                 feature_map, 
-                 model_id="DNN", 
-                 gpu=-1, 
-                 learning_rate=1e-3, 
-                 embedding_dim=10, 
-                 hidden_units=[64, 64, 64], 
-                 hidden_activations="ReLU", 
-                 net_dropout=0, 
-                 batch_norm=False, 
-                 embedding_regularizer=None, 
+    def __init__(self,
+                 feature_map,
+                 model_id="DNN",
+                 gpu=-1,
+                 learning_rate=1e-3,
+                 select_num=0,
+                 embedding_dim=10,
+                 hidden_units=[64, 64, 64],
+                 hidden_activations="ReLU",
+                 net_dropout=0,
+                 batch_norm=False,
+                 embedding_regularizer=None,
                  net_regularizer=None,
                  **kwargs):
         super(DNN, self).__init__(feature_map, 
@@ -60,6 +64,11 @@ class DNN(BaseModel):
                              output_activation=self.output_activation,
                              dropout_rates=net_dropout,
                              batch_norm=batch_norm)
+        if select_num > 0:
+            self.controller = Mv.MvFS_Controller(input_dim=get_sum_feature_dimisions(self.embedding_layer),
+                                                 embed_dims=len(self.feature_map.features), num_selections=select_num)
+        self.weight = 0
+
         self.compile(kwargs["optimizer"], kwargs["loss"], learning_rate)
         self.reset_parameters()
         self.model_to_device()
@@ -99,6 +108,19 @@ class DNN(BaseModel):
         y_pred = self.mlp(feature_emb)
         return_dict = {"y_pred": y_pred}
         return return_dict
+
+    def forward_with_mvfs(self,inputs):
+        X = self.get_inputs(inputs)
+        feature_emb = self.embedding_layer(X)
+        # The embedding process for X will be in MvFS
+        self.weight = self.controller(feature_emb)
+        selected_field = feature_emb * torch.unsqueeze(self.weight, 2)
+
+        feature_emb = selected_field.flatten(start_dim=1)
+        y_pred = self.mlp(feature_emb)
+        return_dict = {"y_pred": y_pred}
+        return return_dict
+
 
     def fit_for_autofield(self, data_generator, epochs=1, validation_data=None,
                           max_gradient_norm=10., **kwargs):
@@ -295,6 +317,67 @@ class DNN(BaseModel):
                                                   'feature_weight': gates_theta.tolist(),
                                                   'feature_sigma': gates_sigma.tolist()})
         feature_importance_result.to_csv('feature_importance_result.csv', index=False)
+
+    def fit_for_mvfs(self, data_generator, epochs=1, validation_data=None,
+            max_gradient_norm=10., **kwargs):
+        self.valid_gen = validation_data
+        self._max_gradient_norm = max_gradient_norm
+        self._best_metric = np.Inf if self._monitor_mode == "min" else -np.Inf
+        self._stopping_steps = 0
+        self._steps_per_epoch = len(data_generator)
+        self._stop_training = False
+        self._total_steps = 0
+        self._batch_index = 0
+        self._epoch_index = 0
+        if self._eval_steps is None:
+            self._eval_steps = self._steps_per_epoch
+
+        logging.info("Start training: {} batches/epoch".format(self._steps_per_epoch))
+        logging.info("************ Epoch=1 start ************")
+        for epoch in range(epochs):
+            self._epoch_index = epoch
+            self._batch_index = 0
+            train_loss = 0
+            self.train()
+            if self._verbose == 0:
+                batch_iterator = data_generator
+            else:
+                batch_iterator = tqdm(data_generator, disable=False, file=sys.stdout)
+            for batch_index, batch_data in enumerate(batch_iterator):
+                self._batch_index = batch_index
+                self._total_steps += 1
+
+                return_dict = self.forward_with_mvfs(batch_data)
+                # --- update for droprank end---
+
+                self.optimizer.zero_grad()
+
+                y_true = self.get_labels(batch_data)
+                loss = self.compute_loss(return_dict, y_true)
+                # --- update for droprank end---
+                loss.backward()
+                # # --- update for droprank start---
+                # print("\n",gates_theta.grad,"\n")
+                # # --- update for droprank end---
+
+                nn.utils.clip_grad_norm_(self.parameters(), self._max_gradient_norm)
+                self.optimizer.step()
+
+                train_loss += loss.item()
+                if self._total_steps % self._eval_steps == 0:
+                    logging.info("Train loss: {:.6f}".format(train_loss / self._eval_steps))
+                    train_loss = 0
+                    # eval the model
+                    self.eval_step()
+                if self._stop_training:
+                    break
+            if self._stop_training:
+                break
+            else:
+                logging.info("************ Epoch={} end ************".format(self._epoch_index + 1))
+        logging.info("Training finished.")
+        logging.info("Load best model: {}".format(self.checkpoint))
+        self.load_weights(self.checkpoint)
 
     def weight_watcher(self):
         # TODO: run in debug mode
