3be9a85850550561b7063a623a871f6a1f3807d0
Wang Xianquan <wxq2001@gmail.com>
2023-12-31 17:03:32 +0800

    <feat> update DNN-mvfs

--- model_zoo/DNN/DNN_torch/run_expid_mvfs_incre.py
+++ model_zoo/DNN/DNN_torch/run_expid_mvfs_incre.py
@@ -46,6 +46,7 @@ import numpy as np
 import logging
 from infomer import email
 import pandas as pd
+import copy
 
 if __name__ == '__main__':
     ''' Usage: python run_expid.py --config {config_dir} --expid {experiment_id} --gpu {gpu_device_id}
@@ -55,6 +56,7 @@ if __name__ == '__main__':
     parser.add_argument('--expid', type=str, default='DeepFM_test', help='The experiment id to run.')
     parser.add_argument('--gpu', type=int, default=-1, help='The gpu index, -1 for cpu')
     parser.add_argument('--cp', type=str, help='checkpoint path')
+    parser.add_argument('--epoch_pre', type=int, default=2, nargs='+', help='pretrain/main_train epochs')
     args = vars(parser.parse_args())
 
     experiment_id = args['expid']
@@ -82,35 +84,41 @@ if __name__ == '__main__':
     feature_map = FeatureMap(params['dataset_id'], data_dir)
     feature_map.load(feature_map_json, params)
 
+    logging.info('****** Warmup network without controller ******')
+    warmup_path = f"{params['model']}_{params['dataset_id']}_warmup4mv.pth"
+    model_class = getattr(model_zoo, params['model'])
+    model = model_class(feature_map, **params)
+    model.count_parameters()
+    train_gen, valid_gen = H5DataLoader(feature_map, stage='train', **params).make_iterator()
+    params_pretrain = copy.deepcopy(params)
+    params_pretrain['epochs'] = args['epoch_pre']
+    model.fit(train_gen, validation_data=valid_gen, **params_pretrain)
+    torch.save(model.state_dict(), warmup_path)
+    logging.info('****** Warmup end and save model in {} ******'.format(warmup_path))
+    del train_gen, valid_gen
+
     select_nums = []
     AUCs = []
     logloss = []
-    for i in range(0, len(feature_map.features)):
+    time_consumption = []
+    incre = 1
+    for i in range(0 + incre - 1, len(feature_map.features) + incre - 1, incre):
         model_class = getattr(model_zoo, params['model'])
         print('params[model]', params['model'])
-        model = model_class(feature_map, select_num = i+1,**params)
-        select_nums.append(i+1)
+        model = model_class(feature_map, select_num=i + 1, **params)
+        model.load_state_dict(torch.load(warmup_path), strict=False)
+        select_nums.append(i + 1)
         model.count_parameters()  # print number of parameters used in model
+        logging.info('Used Feature Number:{} '.format(i + 1))
 
-        print('Used Feature Number:', i + 1)
         train_gen, valid_gen = H5DataLoader(feature_map, stage='train', **params).make_iterator()
-        # email.common_send('AdaFS_select.py - Build Data',"")
-        if args.get('cp',None) != None:
+        if args.get('cp', None) != None:
             print('load model from checkpoint')
             model.load_state_dict(torch.load(args['cp']))
         else:
+            start_time = datetime.now()
             model.fit_for_mvfs(train_gen, validation_data=valid_gen, **params)
-
-        # email.common_send('AdaFS_{}_select.py - Training', "".format(args['expid']))
-
-        logging.info('****** Validation evaluation ******')
-        valid_result = model.evaluate(valid_gen)
-        del train_gen, valid_gen
-        gc.collect()
-
-        logging.info('****** MvFs Validation evaluation ******')
-        train_gen, valid_gen = H5DataLoader(feature_map, stage='train', **params).make_iterator()
-        model.evaluate(valid_gen)
+            time_consumption.append((datetime.now() - start_time).total_seconds())
         del train_gen, valid_gen
         gc.collect()
 
@@ -122,9 +130,10 @@ if __name__ == '__main__':
             AUCs.append(test_result['AUC'])
             logloss.append(test_result['logloss'])
         del test_gen
-
+        gc.collect()
     # Save the result
     topk_ablation_df = pd.DataFrame({'feature_num': select_nums,
                                      'topk_logloss': logloss,
-                                     'topk_auc': AUCs})
+                                     'topk_auc': AUCs,
+                                     'time_consumption': time_consumption})
     topk_ablation_df.to_csv('feature_ablation.csv')
